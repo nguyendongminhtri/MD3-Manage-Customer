d46e4c9e6f6f1ae70f620d03246d21d1cd2a3f71
unknown <nguyendongminhtri29092014@gmail.com>
2021-04-01 18:27:08 +0700

    manage customer

---
+++ src/main/java/com/example/manageCustomer/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.manageCustomer.service;
+
+import com.example.manageCustomer.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
